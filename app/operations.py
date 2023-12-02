3c72b0701c38c394886007b0aeecfc9d17ee1efc
Rafael <rafarbop@gmail.com>
2023-12-02 17:57:41 -0300

    config: Initial configs for project Python

---
+++ app/operations.py
@@ -0,0 +1,11 @@
+def soma(a, b):
+    """Summary or Description of the Function.
+
+    Parameters:
+    a (int): first number
+    b (int): second number
+
+    Returns:
+    int: Returning value
+    """
+    return a + b
